2414a61dfb9cf83a4005b341bf98b05f0bd0a809
simonemadau <simonemadau@yahoo.it>
2015-06-21 18:50:13 +0200

    db

--- simone/pagine/configurazione.php
+++ simone/pagine/configurazione.php
@@ -2,7 +2,7 @@
 define("COSTANTE_HOST","localhost");
 define("COSTANTE_UTENTE","simoneMadau");
 define("COSTANTE_PASSWORD","pipistrello896");
-define("COSTANTE_DATABASE","fitness");
+define("COSTANTE_DATABASE","amm14_simoneMadau");
 $connessione=mysql_connect(COSTANTE_HOST,COSTANTE_UTENTE,COSTANTE_PASSWORD) 
   or die("Problemi di connessione: controlla le credenziali");
 mysql_select_db(COSTANTE_DATABASE) or die("Database non presente");
